6735ee6880a6cf9823fe7376bdb8f4179160b0ac
CF Kosta <cfqa@CFs-MacBook-Pro.local>
2024-06-29 20:45:30 +0300

    Added some more info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (longitude, latitude, callback)=>{
             callback('Unable to find location', undefined)
         }
         else{
-            callback(undefined,body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like '+ body.current.feelslike +' degress out')
+            callback(undefined,body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like '+ body.current.feelslike +' degress out'+ 'the humidity percentage is '+ body.current.humidity)
         }
     })
 }
